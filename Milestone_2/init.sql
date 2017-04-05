be63048273a982681adddd7a08eca74ac2d61d2e
Niroshan Vijayarasa <niroshan.vijayarasa@epfl.ch>
2017-04-05 20:04:33 +0200

    Don't know yet how to declare and use the variable. Add a test file to ensure that the final sql file will work

--- Milestone_2/init.sql
+++ Milestone_2/init.sql
@@ -19,9 +19,8 @@ SET @story_reprint = @data_folder_name + 'story_reprint.csv' + @clean_extension;
 
 /*
 TODO:
-	- deal with cyclical reference.
-    - How to choose the size of the CHAR type in order to be OPTIMAL.
-    - WRITE sql file to load files.
+    - How to choose the size of the CHAR type in order to be OPTIMAL. What type to choose to DATE  .
+    - write sql file to LOAD cleaned  files.
     
 */
 
@@ -35,91 +34,33 @@ TODO:
 
 -- Part 1 is to create the table schema.
 
-CREATE TABLE Story (
-	id INTEGER,
-	title TEXT,
-	feature CHAR(64),
-	issue_id INTEGER, -- Certaines ne le contienne pas donc a voir si on laisse NOT NULL
-	script TEXT,
-	pencils CHAR(64),
-	inks CHAR(64),
-	colors CHAR(64),
-	letters CHAR(64),
-	editing TEXT,
-	genre CHAR(64),
-	characters TEXT,
-	synopsis TEXT,
-	reprint_notes TEXT,
-	notes TEXT,
-	type_id INTEGER NOT NULL, -- Je l'ai presque tout le temps vu on pourrait le garder not null
-	PRIMARY KEY (id),
-	FOREIGN KEY (issue_id) REFERENCES Issue(id),
-	FOREIGN KEY (type_id) REFERENCES Story_Type(id)
-);
-
-CREATE TABLE Series (
+CREATE TABLE Story_Type (
 	id INTEGER,
 	name TEXT,
-	format TEXT, -- rarely appears (almost never)
-	year_began INTEGER,
-	year_ended INTEGER,
-	publication_dates CHAR(64),
-	first_issue_id INTEGER NOT NULL,
-	last_issue_id INTEGER NOT NULL,
-	publisher_id INTEGER,
-	country_id INTEGER NOT NULL,
-	language_id INTEGER,
-	notes TEXT,
-	color TEXT,
-	dimensions TEXT,
-	paper_stock TEXT,
-	binding TEXT,
-	publishing_format TEXT,
-	publication_type_id INTEGER,
-	PRIMARY KEY (id),
-	FOREIGN KEY (first_issue_id) REFERENCES Issue(id),
-	FOREIGN KEY (last_issue_id) REFERENCES Issue(id),
-	FOREIGN KEY (publisher_id) REFERENCES Publisher(id),
-	FOREIGN KEY (language_id) REFERENCES Language(id),
-	FOREIGN KEY (country_id) REFERENCES Country(id)
+	PRIMARY KEY (id)
 );
 
-CREATE TABLE Issue (
+CREATE TABLE Series_Publication_Type (
 	id INTEGER,
-	issue_number CHAR(32),
-	series_id INTEGER,
-	indicia_publisher_id INTEGER, -- there was NOT NULL here but in the file, it's almost always NULL
-	publication_date CHAR(64), -- for example "monthly"
-	price TEXT,
-	page_count INTEGER,
-	indicia_frequency CHAR(64),
-	editing TEXT,
-	notes TEXT,
-	isbn TEXT, -- almost never appers, I put TEXT to be safe (because I didn't see any isbn in the file)
-	valid_isbn TEXT, -- almost never appers, I put TEXT to be safe (because I didn't see any valid_isbn in the file)
-	barcode TEXT, -- almost never appers, I put TEXT to be safe (because I didn't see any barcode in the file)
-	title TEXT, -- almost never appers, I put TEXT to be safe (because I didn't see any title in the file)
-	on_sale_date DATE, -- or CHAR(20) for example (but in the file, it was YYYY-MM-DD)
-	rating INTEGER, -- almost never appers, I put TEXT to be safe (because I didn't see any isbn in the file)
-	PRIMARY KEY (id),
-	FOREIGN KEY (series_id) REFERENCES Series(id),
-	FOREIGN KEY (indicia_publisher_id) REFERENCES Indicia_Publisher(id)
+	name CHAR(16), -- maximum est 8 pour le moment mais permet d'avoir une marge.
+	PRIMARY KEY (id)
 );
 
-CREATE TABLE Indicia_Publisher (
+CREATE TABLE Country (
 	id INTEGER,
+	code CHAR(8), -- code max length is 4 in the file but we prefer to be safe
 	name TEXT,
-	publisher_id INTEGER NOT NULL, -- Toujours vu: devrait laisser not null
-	country_id INTEGER NOT NULL, -- Pareil presque toujours vu
-	year_began INTEGER, -- Des fois ils écrivent NULL a la place de laisser blanc: faudrait effacer tous les NULL
-	year_ended INTEGER, -- Des fois ils écrivent NULL a la place de laisser blanc: faudrait effacer tous les NULL
-	is_surrogate INTEGER, -- Always 0 or 1
-	notes TEXT,
-	url CHAR(256),
-	PRIMARY KEY (id),
-	FOREIGN KEY (publisher_id) REFERENCES Publisher(id),
-	FOREIGN KEY (country_id) REFERENCES Country(id)
+	PRIMARY KEY (id)
 );
+
+CREATE TABLE Language (
+	id INTEGER,
+	code CHAR(8), -- code max length is 4 in the file but we prefer to be safe
+	name TEXT,
+	PRIMARY KEY (id)
+);
+
+
 
 CREATE TABLE Publisher (
 	id INTEGER,
@@ -145,31 +86,94 @@ CREATE TABLE Brand_Group (
 	FOREIGN KEY (publisher_id) REFERENCES Publisher(id)
 );
 
-CREATE TABLE Country (
-	id INTEGER,
-	code CHAR(8), -- code max length is 4 in the file but we prefer to be safe
-	name TEXT,
-	PRIMARY KEY (id)
-);
-
-CREATE TABLE Language (
-	id INTEGER,
-	code CHAR(8), -- code max length is 4 in the file but we prefer to be safe
-	name TEXT,
-	PRIMARY KEY (id)
-);
-
-CREATE TABLE Series_Publication_Type (
-	id INTEGER,
-	name CHAR(16), -- maximum est 8 pour le moment mais permet d'avoir une marge.
-	PRIMARY KEY (id)
-);
-
-CREATE TABLE Story_Type (
+CREATE TABLE Indicia_Publisher (
 	id INTEGER,
 	name TEXT,
-	PRIMARY KEY (id)
+	publisher_id INTEGER NOT NULL, -- Toujours vu: devrait laisser not null
+	country_id INTEGER NOT NULL, -- Pareil presque toujours vu
+	year_began INTEGER, -- Des fois ils écrivent NULL a la place de laisser blanc: faudrait effacer tous les NULL
+	year_ended INTEGER, -- Des fois ils écrivent NULL a la place de laisser blanc: faudrait effacer tous les NULL
+	is_surrogate INTEGER, -- Always 0 or 1
+	notes TEXT,
+	url CHAR(256),
+	PRIMARY KEY (id),
+	FOREIGN KEY (publisher_id) REFERENCES Publisher(id),
+	FOREIGN KEY (country_id) REFERENCES Country(id)
 );
+
+CREATE TABLE Issue (
+	id INTEGER,
+	issue_number CHAR(32),
+	series_id INTEGER,
+	indicia_publisher_id INTEGER, -- there was NOT NULL here but in the file, it's almost always NULL
+	publication_date CHAR(64), -- for example "monthly"
+	price TEXT,
+	page_count INTEGER,
+	indicia_frequency CHAR(64),
+	editing TEXT,
+	notes TEXT,
+	on_sale_date DATE, -- or CHAR(20) for example (but in the file, it was YYYY-MM-DD)
+	PRIMARY KEY (id),
+	-- FOREIGN KEY (series_id) REFERENCES Series(id),  BECAUSE CIRCULAR REFERENCES
+	FOREIGN KEY (indicia_publisher_id) REFERENCES Indicia_Publisher(id)
+);
+
+CREATE TABLE Series (
+	id INTEGER,
+	name TEXT,
+	year_began INTEGER,
+	year_ended INTEGER,
+	publication_dates CHAR(64),
+	first_issue_id INTEGER NOT NULL,
+	last_issue_id INTEGER NOT NULL,
+	publisher_id INTEGER,
+	country_id INTEGER NOT NULL,
+	language_id INTEGER,
+	notes TEXT,
+	color TEXT,
+	dimensions TEXT,
+	paper_stock TEXT,
+	binding TEXT,
+	publishing_format TEXT,
+	publication_type_id INTEGER,
+	PRIMARY KEY (id),
+	FOREIGN KEY (first_issue_id) REFERENCES Issue(id),
+	FOREIGN KEY (last_issue_id) REFERENCES Issue(id),
+	FOREIGN KEY (publisher_id) REFERENCES Publisher(id),
+	FOREIGN KEY (language_id) REFERENCES Language(id),
+	FOREIGN KEY (country_id) REFERENCES Country(id)
+);
+
+
+-- ADD the remove foreign key because of the circular reference.
+ALTER TABLE Issue
+ADD FOREIGN KEY (series_id) REFERENCES Series(id);  
+
+
+
+
+CREATE TABLE Story (
+	id INTEGER,
+	title TEXT,
+	feature CHAR(64),
+	issue_id INTEGER, -- Certaines ne le contienne pas donc a voir si on laisse NOT NULL
+	script TEXT,
+	pencils CHAR(64),
+	inks CHAR(64),
+	colors CHAR(64),
+	letters CHAR(64),
+	editing TEXT,
+	genre CHAR(64),
+	characters TEXT,
+	synopsis TEXT,
+	reprint_notes TEXT,
+	notes TEXT,
+	type_id INTEGER NOT NULL, -- Je l'ai presque tout le temps vu on pourrait le garder not null
+	PRIMARY KEY (id),
+	FOREIGN KEY (issue_id) REFERENCES Issue(id),
+	FOREIGN KEY (type_id) REFERENCES Story_Type(id)
+);
+
 
 CREATE TABLE Story_Reprint (
 	id INTEGER,
