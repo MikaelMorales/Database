dcba30a2f9ffcb5008438bae3b7b2470ad64fe50
Charles Parzy Turlat <charles.parzy-turlat@epfl.ch>
2017-04-08 12:17:43 +0200

    add script counting the max  nb of char per attributes

---
+++ scripts/attributes_size.py
@@ -0,0 +1,24 @@
+import csv
+import re
+
+filename = input('Enter the filename : ');
+
+attributeNames = []
+attributeSizeCnt = {}
+
+with open(filename, 'r') as f:
+	reader = csv.reader(f)
+	attributeNames = next(reader)
+
+for attribute in attributeNames:
+	attributeSizeCnt.update({attribute: 0})
+
+with open(filename, 'r') as f:
+	dictReader = csv.DictReader(f, quoting=csv.QUOTE_NONE)
+	for row in dictReader:
+		for attribute in attributeNames:
+			if len(row[attribute]) > attributeSizeCnt[attribute]:
+				attributeSizeCnt[attribute] = len(row[attribute])
+
+for attribute in attributeNames:
+	print("""{} => {}""".format(attribute, attributeSizeCnt[attribute]))
