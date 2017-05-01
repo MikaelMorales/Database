0e268715de541bf0b261a64ac0b09151fec674f1
Charles Parzy Turlat <charles.parzy-turlat@epfl.ch>
2017-05-01 10:19:22 +0200

    clean repo

--- Milestone_2/request.sql
+++ Milestone_2/request.sql
@@ -61,3 +61,22 @@ WHERE S.title IS NOT NULL AND S.id = SR.origin_id
 GROUP BY SR.origin_id 
 ORDER BY COUNT(*) DESC
 LIMIT 10;
+
+
+-- Print the artists that have scripted, drawn, and colored at least one of the stories they were involved in.
+
+SELECT name FROM Story_Artists A, (SELECT DISTINCT C.artist_id FROM Story_Has_Scripts H, Story_Has_Colors C, `Story_Has_Pencils` P    WHERE  
+H.`story_id` = C.`story_id` AND C.`story_id` = P.`story_id`  AND
+H.`artist_id` = C.`artist_id` AND C.`artist_id` = P.`artist_id`) X WHERE A.id = X.`artist_id`;
+
+-- 4150 line.
+
+-- Print all non-reprinted stories involving Batman as a non-featured character.
+
+-- VERSION 1
+SELECT DISTINCT  S.title FROM Story S
+WHERE S.id NOT IN
+(SELECT DISTINCT X.story_id FROM `Story_Reprint` R, (SELECT DISTINCT story_id FROM Story_Has_Characters H, `Story_Characters` C WHERE C.`name` = 'Batman') X WHERE  
+(R.origin_id = X.story_id OR R.target_id = X.story_id));
+
+-- 823303 line
