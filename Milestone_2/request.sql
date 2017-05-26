523601c20d98af67e5e35bf6a5801c861ca11cf9
Charles Parzy Turlat <charles.parzy-turlat@epfl.ch>
2017-05-26 10:14:32 +0200

    correction request2

--- Milestone_2/request.sql
+++ Milestone_2/request.sql
@@ -1,26 +1,26 @@
 -- Print the brand group names with the highest number of Belgian indicia publishers.
 SELECT G.name
 FROM Brand_Group G
-WHERE G.publisher_id IN 
+WHERE G.publisher_id IN
 			(SELECT I2.publisher_id
 			 FROM Indicia_Publisher I2, Country C1
 			 WHERE C1.name = "Belgium" AND I2.country_id = C1.id
 			 GROUP BY I2.publisher_id
-			 HAVING COUNT(*) >= 
+			 HAVING COUNT(*) >=
 			 	(SELECT MAX(counted) FROM (SELECT COUNT(I3.id) AS counted
 										   FROM Indicia_Publisher I3, Country C2
 										   WHERE C2.name = "Belgium" AND I3.country_id = C2.id
 										   GROUP BY I3.publisher_id) AS counts));
-										   
--- Print the ids and names of publishers of Danish book series.								   
+
+-- Print the ids and names of publishers of Danish book series.
 SELECT DISTINCT(P.id), P.name
 FROM Publisher P
 WHERE P.id IN (SELECT S.publisher_id
-			   FROM Series S, Country C
-			   WHERE C.name = "Denmark" AND S.country_id = C.id);
+			   FROM Series S, Country C, Series_Publication_Type SPT
+			   WHERE C.name = "Denmark" AND S.country_id = C.id AND SPT.name = "book" AND S.publication_type_id = SPT.id);
 
 -- Print the ids and names of publishers of Danish book series.
--- But slower.		   
+-- But slower.
 SELECT DISTINCT(P.id), P.name
 FROM Publisher P JOIN Series S, Language L
 WHERE P.id = S.publisher_id AND L.name = "Danish" AND S.language_id = L.id;
@@ -39,14 +39,14 @@ GROUP BY I.publication_date;
 -- Print the number of series for each indicia publisher whose name resembles ‘DC comics’.
 SELECT I.indicia_publisher_id, IndiPubli.name, COUNT(DISTINCT(I.series_id))
 FROM Issue I, Indicia_Publisher IndiPubli
-WHERE I.indicia_publisher_id = IndiPubli.id AND IndiPubli.name LIKE 'DC comics'
+WHERE I.indicia_publisher_id = IndiPubli.id AND IndiPubli.name LIKE "%DC comics%"
 GROUP BY I.indicia_publisher_id;
 
 -- Print the titles of the 10 most reprinted stories
-SELECT S.title, count FROM 
-(SELECT SR.origin_id, COUNT(*) AS count 
- FROM Story_Reprint SR 
- GROUP BY SR.origin_id 
+SELECT S.title, count FROM
+(SELECT SR.origin_id, COUNT(*) AS count
+ FROM Story_Reprint SR
+ GROUP BY SR.origin_id
  ORDER BY COUNT(*) DESC) S1
 JOIN Story S
 ON S.id = S1.origin_id
@@ -58,29 +58,29 @@ LIMIT 10;
 SELECT S.title, COUNT(*)
 FROM Story_Reprint SR, Story S
 WHERE S.title IS NOT NULL AND S.id = SR.origin_id
-GROUP BY SR.origin_id 
+GROUP BY SR.origin_id
 ORDER BY COUNT(*) DESC
 LIMIT 10;
 
 
 -- Print the artists that have scripted, drawn, and colored at least one of the stories they were involved in.
-SELECT name 
+SELECT name
 FROM Story_Artists A
-WHERE A.id IN (SELECT DISTINCT C.artist_id 
-			   FROM Story_Has_Scripts H, Story_Has_Colors C, Story_Has_Pencils P    
-			   WHERE H.`story_id` = C.`story_id` AND 
-		  		     C.`story_id` = P.`story_id`  AND 
-		  			 H.`artist_id` = C.`artist_id` AND  
+WHERE A.id IN (SELECT DISTINCT C.artist_id
+			   FROM Story_Has_Scripts H, Story_Has_Colors C, Story_Has_Pencils P
+			   WHERE H.`story_id` = C.`story_id` AND
+		  		     C.`story_id` = P.`story_id`  AND
+		  			 H.`artist_id` = C.`artist_id` AND
 		 			 C.`artist_id` = P.`artist_id`);
 
 -- Print all non-reprinted stories involving Batman as a non-featured character.
 SELECT DISTINCT S.title
 FROM Story_Characters SC, Story_Has_Characters SHC, Story S
-WHERE SC.name = "Batman" AND 
-	  SHC.character_id = SC.id AND 
-	  S.id = SHC.story_id AND 
-	  S.title IS NOT NULL AND 
+WHERE SC.name = "Batman" AND
+	  SHC.character_id = SC.id AND
+	  S.id = SHC.story_id AND
+	  S.title IS NOT NULL AND
 	  S.id NOT IN (SELECT SR.origin_id FROM Story_Reprint SR) AND
-	  S.id NOT IN (SELECT DISTINCT SHF.story_id 
+	  S.id NOT IN (SELECT DISTINCT SHF.story_id
 				   FROM Story_Has_Features SHF, Story_Features SF
 				   WHERE SF.name = "Batman" AND SHF.feature_id = SF.id);
