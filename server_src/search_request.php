cd4ef344a3cbb2e355b08c0e18dcd85375ff2d79
Charles Parzy Turlat <charles.parzy-turlat@epfl.ch>
2017-06-15 00:09:56 +0200

    refactoring server

--- server_src/search_request.php
+++ server_src/search_request.php
@@ -7,11 +7,11 @@
 	if ($tables[0] == null) {
 		$tables = TablesInformations::getTables();
 	}
-	// $limit = $data["MaxLineNb"];
-	$limit = 2;
+	$limit = $data["MaxLineNb"];
+	// $limit = 2;
 
-	// $offset = $data["Offset"];
-	$offset = 0;
+	$offset = $data["Offset"];
+	// $offset = 0;
 
 	$connection_mysql = Utils::connect_to_db();
 
@@ -21,54 +21,25 @@
 
 	function makeSQLQuery($tables, $requestedAttributes, $connection_mysql, $offset, $limit) {
 		if ($requestedAttributes != null) {
-			if (is_numeric($requestedAttributes)) {
-				// queryID($tables, $requestedAttributes, $connection_mysql);
-				queryID($tables, $requestedAttributes, $connection_mysql, $offset, $limit);
-			} else {
-				queryOnStandardAttributes($tables, $requestedAttributes, $connection_mysql, $offset, $limit);
+			$tableToRows = [];
+			foreach ($tables as $table) {
+				$attributes;
+				if (is_numeric($requestedAttributes)) {
+					$attributes = TablesInformations::getNumericAttributes($table);
+				} else {
+					$attributes = TablesInformations::getAttributesOfTable($table);
+				}
+				$request = "SELECT * FROM " . $table . " WHERE " . $attributes[0] . " LIKE \"" . $requestedAttributes . "\"";
+				unset($attributes[0]);
+				foreach ($attributes as $attribute) {
+					$request .= " OR " . $attribute . " LIKE \"" . $requestedAttributes ."\"";
+				}
+				$request .= "LIMIT " . $limit . " OFFSET " . $offset . ";";
+
+				// error_log($request, 3, "te.txt");
+				$tableToRows = Utils::execute_request($connection_mysql, $request, $tableToRows, $table, TablesInformations::getNicerNames($table));
 			}
+			echo json_encode($tableToRows);
 		}
-	}
-
-	// function queryID($tables, $requestedAttributes, $connection_mysql) {
-	// 	$tableToRows = [];
-	// 	foreach ($tables as $table) {
-	// 			$request = "SELECT * FROM " . $table . " WHERE id = " . $requestedAttributes . ";";
-	// 			$tableToRows = Utils::execute_request($connection_mysql, $request, $tableToRows, $table);
-	// 	}
-	// 	echo json_encode($tableToRows);
-	// }
-
-	function queryID($tables, $requestedAttributes, $connection_mysql, $offset, $limit) {
-		$tableToRows = [];
-		foreach ($tables as $table) {
-			$attributes = TablesInformations::getNumericAttributes($table);
-			$request = "SELECT * FROM " . $table . " WHERE " . $attributes[0] . " LIKE \"" . $requestedAttributes . "\"";
-			unset($attributes[0]);
-			foreach ($attributes as $attribute) {
-				$request .= " OR " . $attribute . " LIKE \"" . $requestedAttributes ."\"";
-			}
-			$request .= "LIMIT " . $limit . " OFFSET " . $offset . ";";
-
-			// error_log($request, 3, "te.txt");
-			$tableToRows = Utils::execute_request($connection_mysql, $request, $tableToRows, $table, TablesInformations::getNicerNames($table));
-		}
-		echo json_encode($tableToRows);
-	}
-
-	function queryOnStandardAttributes($tables, $requestedAttributes, $connection_mysql, $offset, $limit) {
-		$tableToRows = [];
-		foreach ($tables as $table) {
-			$attributes = TablesInformations::getAttributesOfTable($table);
-			$request = "SELECT * FROM " . $table . " WHERE " . $attributes[0] . " LIKE \"" . $requestedAttributes . "\"";
-			unset($attributes[0]);
-			foreach ($attributes as $attribute) {
-				$request .= " OR " . $attribute . " LIKE \"" . $requestedAttributes ."\"";
-			}
-			$request .= "LIMIT " . $limit . " OFFSET " . $offset . ";";
-
-			$tableToRows = Utils::execute_request($connection_mysql, $request, $tableToRows, $table, TablesInformations::getNicerNames($table));
-		}
-		echo json_encode($tableToRows);
 	}
 ?>
